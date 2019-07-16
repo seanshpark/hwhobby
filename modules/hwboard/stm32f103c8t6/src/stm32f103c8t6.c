5910688e2549e76bf607e9490f90671b70b00441
SaeHie Park <saehie.park@gmail.com>
2019-07-16 22:20:03 +0900

    add stm copyright

--- modules/hwboard/stm32f103c8t6/src/stm32f103c8t6.c
+++ modules/hwboard/stm32f103c8t6/src/stm32f103c8t6.c
@@ -14,6 +14,21 @@
  * limitations under the License.
  */
 
+/******************** (C) COPYRIGHT 2007 STMicroelectronics ********************
+* File Name          : main.c
+* Author             : MCD Application Team
+* Version            : V1.0
+* Date               : 10/08/2007
+* Description        : Main program body
+********************************************************************************
+* THE PRESENT SOFTWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+* WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE TIME.
+* AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY DIRECT,
+* INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING FROM THE
+* CONTENT OF SUCH SOFTWARE AND/OR THE USE MADE BY CUSTOMERS OF THE CODING
+* INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+*******************************************************************************/
+
 #include <stm32f10x_lib.h>
 
 #include "stm32f103c8t6.h"
